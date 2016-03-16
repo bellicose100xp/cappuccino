d5ed8c296f4b769840ea85b2d37dc16e009a1356
bellicose <bellicose.agnostic@gmail.com>
2016-03-16 09:19:43 -0700

    before redux.. with flexbox

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,25 @@ export default () => {
     return (
         <div className="main-container">
             <div className="my-head">Cappuccino Cat</div>
+            <div className="food-list-container">
+                <div className="food-item-container">
+                    <div className="food-item">Item1</div>
+                </div>
+                <div className="food-item-container">
+                    <div className="food-item">Item2</div>
+                </div>
+                <div className="food-item-container">
+                    <div className="food-item">Item3</div>
+                </div>
+                <div className="food-item-container">
+                    <div className="food-item">Item4</div>
+                </div>
+                <div className="food-item-container">
+                    <div className="food-item">Item5</div>
+                </div>
+                <div className="food-item-container">
+                </div>
+            </div>
         </div>
     )
 }
