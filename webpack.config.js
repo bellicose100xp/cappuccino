6ef47425132d6b0df99252f171a9a9f9958a4e9c
bellicose <bellicose.agnostic@gmail.com>
2016-03-23 09:43:48 -0700

    added recipe modify section..

--- webpack.config.js
+++ webpack.config.js
@@ -40,22 +40,22 @@ module.exports = {
             {
                 test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: "file"
+                loader: "file?name=fonts/[name].[ext]"
             },
             {
                 test: /\.(woff|woff2)$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: "url?prefix=font/&limit=5000"
+                loader: "url?prefix=font/&limit=5000&name=fonts/[name].[ext]"
             },
             {
                 test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: "url?limit=10000&mimetype=application/octet-stream"
+                loader: "url?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]"
             },
             {
                 test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: "url?limit=10000&mimetype=image/svg+xml"
+                loader: "url?limit=10000&mimetype=image/svg+xml&name=fonts/[name].[ext]"
             }
         ]
     }
